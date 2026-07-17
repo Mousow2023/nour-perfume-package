e1c13296aac4146876bf8d6eb7485a253e183cbb
Moustapha SOW <sowmoustapha0730@gmail.com>
2026-07-17 22:36:29 +0000

    Configure deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './',
+  base: 'nour-perfume-package',
   plugins: [react(), tailwindcss()],
   build: {
     outDir: 'dist',
